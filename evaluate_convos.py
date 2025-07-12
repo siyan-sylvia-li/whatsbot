5ff18f6d19b51e0ef70192745c9201410f3e5496
Shopnil09 <shopnil.shahriar@gmail.com>
2025-07-12 15:22:55 -0400

    added gemini for evaluation in addition to gpt

--- evaluate_convos.py
+++ evaluate_convos.py
@@ -2,21 +2,37 @@ import openai
 import json
 import os
 import dotenv
+import google.generativeai as genai
 
 dotenv.load_dotenv(".env")
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# for model in genai.list_models():
+#     print(f"Model Name: {model.name}")
+#     print(f"  Description: {model.description}")
+#     print(f"  Supported Generation Methods: {model.supported_generation_methods}")
+#     print("-" * 30)
+
 client = openai.OpenAI()
 
-def load_prompt_template(path="empathy-evaluation-prompt.txt"): 
+def load_prompt_template(model_name): 
+  path=None
+  if model_name == "gemini": 
+    path="empathy-evaluation-prompt-gemini.txt"
+  else: # gemini
+    path="empathy-evaluation-prompt.txt"
+  
   with open(path, "r", encoding="utf-8") as f: 
-    return f.read()
+      return f.read()
 
 # static items required for conversation evaluation
-MODEL = "gpt-4o"
+MODEL = "gpt-4.1"
 INPUT_ROOT = os.path.join(os.getcwd(), "bot_chats")
 OUTPUT_ROOT = os.path.join(os.getcwd(), "evaluated_bot_chats")
-PROMPT_TEMPLATE = load_prompt_template()
+# change this to "gpt" or "gemini" to load LLM specific prompt
+PROMPT_TEMPLATE = load_prompt_template("gemini")
 
 def format_conversation(convo_list): 
   lines = []
@@ -26,7 +42,7 @@ def format_conversation(convo_list):
   
   return "\n".join(lines)
 
-def get_empathy_score(convo_text): 
+def get_empathy_score_gpt(convo_text): 
   try: 
     response = client.chat.completions.create(
       model=MODEL,
@@ -39,6 +55,21 @@ def get_empathy_score(convo_text):
     score_str = response.choices[0].message.content.strip()
     score = int(score_str)
     return max(0, min(score, 5))  # Clamp to [0, 5]
+  except Exception as e: 
+    print(e)
+    return None
+
+def get_empathy_score_gemini(convo_text): 
+  try: 
+    model = genai.GenerativeModel("gemini-1.5-pro-latest")
+    generation_config = {
+      "temperature": 0.0,
+      "max_output_tokens": 5
+    }
+    response = model.generate_content(PROMPT_TEMPLATE.format(conversation=convo_text), generation_config=generation_config)
+    score_str = response.text.strip()
+    score = int(score_str)
+    return max(0, min(score, 5))
   except Exception as e: 
     print(e)
     return None
@@ -58,7 +89,8 @@ def process_round(round_path, round_name):
       if isinstance(convo, list): 
         convo_text = format_conversation(convo)
         print(f"EVALUATING {round_name} | {phone} | {session_key}")
-        score = get_empathy_score(convo_text)
+        # replace this with gpt or gemini depending on the evaluation LLM
+        score = get_empathy_score_gemini(convo_text)
         if score is not None: 
           sessions[f"{session_key}_empathy_score"] = score
   
